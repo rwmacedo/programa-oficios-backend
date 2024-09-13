3754ae0fe8f5559e7963c12ca4cc4e8b12dccb77
Renata Werneck <rwmacedo@gmail.com>
2024-09-13 13:02:07 -0300

    recolocando pasta api

--- ProgramaOficios.Application/Interfaces/Services/IBlobStorageService.cs
+++
@@ -1,8 +0,0 @@
-namespace ProgramaOficios.Application.Interfaces.Services
-{
-    public interface IBlobStorageService
-    {
-        Task<string> UploadFileAsync(Stream fileStream, string fileName);
-        Task<Stream> DownloadFileAsync(string fileName);
-    }
-}
