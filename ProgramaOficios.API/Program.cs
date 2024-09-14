25f9e80b04968b675d20dc246e92ba02cf6042a0
Renata Werneck <rwmacedo@gmail.com>
2024-09-13 22:13:35 -0300

    corrigindo cors2

--- ProgramaOficios.API/Program.cs
+++ ProgramaOficios.API/Program.cs
@@ -1,16 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Azure.Storage.Blobs;
-using dotenv.net;
 using ProgramaOficios.Infrastructure.Context;
 using ProgramaOficios.Infrastructure.IoC;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Carregar variáveis do arquivo .env
-DotEnv.Load();
-
+// Exibir as variáveis de ambiente para depuração
 Console.WriteLine($"SQL_CONNECTION_STRING: {Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING")}");
 Console.WriteLine($"AZURE_BLOB_STORAGE: {Environment.GetEnvironmentVariable("AZURE_BLOB_STORAGE")}");
 
@@ -32,29 +27,38 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Pegar strings de conexão do .env
+// Pegar strings de conexão do ambiente
 var sqlConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING") 
     ?? throw new InvalidOperationException("A string de conexão SQL não foi configurada corretamente.");
 var blobStorageConnectionString = Environment.GetEnvironmentVariable("AZURE_BLOB_STORAGE") 
     ?? throw new InvalidOperationException("A conexão com o Azure Blob Storage não foi configurada corretamente.");
 
-// Configurar o DbContext para o SQL Server
+// Configurar o DbContext para o PostgreSQL
 builder.Services.AddDbContext<OficioDbContext>(options =>
-    options.UseSqlServer(sqlConnectionString));
+    options.UseNpgsql(sqlConnectionString));
 
 // Configurar o BlobServiceClient usando o Configuration diretamente
 builder.Services.AddSingleton(new BlobServiceClient(blobStorageConnectionString));
 
 var app = builder.Build(); 
 
+// Aplicar migrations automaticamente
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<OficioDbContext>();
     dbContext.Database.Migrate();  // Aplica as migrations
 }
 
+// Pegue a porta do ambiente no Heroku
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Urls.Add($"http://*:{port}");
+
+// Adicionar logs para ver o caminho da requisição
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request for {context.Request.Path} received.");
+    await next.Invoke();
+});
 
 // Configuração do middleware
 if (app.Environment.IsDevelopment())
@@ -64,13 +68,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Usar a política de CORS correta ("AllowAngularClient")
+// Usar a política de CORS correta ("AllowHerokuClient")
 app.UseCors("AllowHerokuClient");
 
 app.UseAuthorization();
+
+// Usar o Swagger na produção também
 app.UseSwagger();
-
-
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    c.RoutePrefix = string.Empty;  // Define o Swagger na raiz
+});
 
 app.MapControllers();
 
